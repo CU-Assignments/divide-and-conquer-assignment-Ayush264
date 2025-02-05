3eb4f45a026b7e9401225be25ab480d9ead765ad
Ayush264 <121258575+Ayush264@users.noreply.github.com>
2025-02-05 10:38:16 +0530

    Create longestNiceSubstring.java

---
+++ longestNiceSubstring.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            ch
+            arSet.add(s.charAt(i));
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charSet.contains(Character.toUpperCase(s.charAt(i))) &&
+                    charSet.contains(Character.toLowerCase(s.charAt(i)))) {
+                continue;
+            }
+            String s1 = longestNiceSubstring(s.substring(0, i));
+            String s2 = longestNiceSubstring(s.substring(i+1));
+            return s1.length()>= s2.length() ? s1 : s2;
+        }
+        return s;
+    }
+}
